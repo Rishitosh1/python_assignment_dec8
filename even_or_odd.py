985ba3677552eb58215f951ad1bd22a83d30aa7b
rishitosh <arishitosh@gmail.com>
2017-12-11 13:26:04 +0545

    to check whether the given number is odd or even

---
+++ even_or_odd.py
@@ -0,0 +1,16 @@
+def even_odd(num):
+
+    if(num%2==0):
+        print("the number is even")
+
+    else:
+        print("the number is odd")
+
+
+num=int(input("enter number to check odd or even: "))
+
+even_odd(num)
+
+
+
+
